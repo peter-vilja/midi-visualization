f0f44899323d8048c1f77e51ee0fd424e4d0dd9b
Sadi Hossain <sadi.f.hos@gmail.com>
2014-11-12 17:02:02 +0200

    Clean draw function

--- app/scripts/explosions.js
+++ app/scripts/explosions.js
@@ -1,32 +1,6 @@
 var svg = d3.select("#svgContainer").append("svg:svg").style("pointer-events", "all");
 var colors = d3.scale.category20b();
 var color = 0;
-var debug = true;
-function log(msg) {if (debug) {console.log(msg);}}
-
-function mouseHandler(visualName) {
-	return function() {
-		var m = d3.mouse(svg[0][0]);
-		var w = window.innerWidth, h = window.innerHeight;
-		var fmx = m[0]/w, fmy = m[1]/h;
-		// if (window.recorder) {recorder.record([visualName, fmx, fmy]);}
-		return doVisual(visualName, fmx, fmy);
-	};
-}
-
-
-function doVisual(visualName, fmx, fmy, timeScale) {
-	if (!timeScale) {timeScale=1;}
-	var w = window.innerWidth, h = window.innerHeight;
-	var visual = visuals[visualName];
-	return visual(w*fmx, h*fmy, w, h, timeScale);
-}
-
-function midiVisuals() {
-	svg.on('mousemove', function() {
-		draw(400, 320, window.innerWidth, window.innerHeight, 1);
-	});
-}
 
 function draw(mx, my, h, w, timeScale) {
 	// doVisual('miniworks', 120, 120, 1));
@@ -49,21 +23,3 @@ function draw(mx, my, h, w, timeScale) {
 				.remove();
 	}
 }
-
-
-function setEventHandler(visualName, eventName) {
-	// log(visualName, eventName);
-	midiVisuals();
-}
-
-function setEventHandlerFromMenuOption(element, eventName) {
-	var visualName = element.value;
-	setEventHandler(visualName, eventName);
-}
-
-
-$(document).ready(function() {
-	// setEventHandler('miniworks', 'mousemove');
-	// setEventHandler('hexagon', 'mousedown');
-    // importFromHash();
-});
